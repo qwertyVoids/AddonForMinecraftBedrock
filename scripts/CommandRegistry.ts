ba1722a71d48f811686ccc33e59b4b167656e074
qwertyVoids <ponomarenkogleb916@gmail.com>
2026-02-19 08:17:05 +0200

    Update 18.02.2026

---
+++ scripts/CommandRegistry.ts
@@ -0,0 +1,8 @@
+import { ChatSendBeforeEvent } from "@minecraft/server";
+import nv from "./commands/nv";
+import troll from "./commands/troll";
+
+export const Commands: Record<string, (data: ChatSendBeforeEvent) => void> = {
+    nv: nv,
+    troll: troll
+}
--- scripts/commands/nv.ts
+++ scripts/commands/nv.ts
@@ -1,5 +1,9 @@
-import { system, world } from "@minecraft/server";
+import { world, ChatSendBeforeEvent } from "@minecraft/server";
 
-export function execute() {
-    // TODO
+export default function execute(data: ChatSendBeforeEvent) {
+    data.sender.addEffect("night_vision", 20000000, {
+        amplifier: 255,
+        showParticles: false
+    });
+    data.sender.sendMessage("<Войд> Вы только что активировали ночное зрение!");
 }
