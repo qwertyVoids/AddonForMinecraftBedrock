5e18b642307abe26f730252caf9f0516f9666cae
Void10100 <ponomarenkogleb916@gmail.com>
2026-02-22 20:54:31 +0200

    Small update: bug fixes in build 1.21.114-1.1.17-stable and small update in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import * as server from "@minecraft/server";
+import { ChatSendBeforeEvent, PlayerSpawnAfterEvent } from "@minecraft/server";
 import { Commands } from "./CommandRegistry";
 import Command from "./Classes/Command";
 
-server.world.beforeEvents.chatSend.subscribe((eventData: server.ChatSendBeforeEvent): void => {
+server.world.beforeEvents.chatSend.subscribe((eventData: ChatSendBeforeEvent): void => {
     if (eventData.message.startsWith("!")) {
         eventData.cancel = true;
 
@@ -16,5 +17,14 @@ server.world.beforeEvents.chatSend.subscribe((eventData: server.ChatSendBeforeEv
         } else {
             eventData.sender.sendMessage("<Войд> Чтобы увидеть список команд, напиши !help");
         }
+    }
+});
+
+server.world.afterEvents.playerSpawn.subscribe(({player, initialSpawn}: PlayerSpawnAfterEvent): void => {
+    if (initialSpawn) {
+        server.system.runTimeout((): void => {
+            player.sendMessage("<Войд> Привет! Я Войд, создатель плагина. Я буду помогать тебе, давая отчёты о результатах использования команд. Напиши !help, чтобы увидеть список всех команд. Кстати, команды, предоставляемые моим плагином, всегда начинаются с восклицательного знака.");
+            player.playSound("note.pling");
+        }, 50);
     }
 });
